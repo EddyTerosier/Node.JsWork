fd78cce474b641e6233fd6ac89afe747e20d243f
Eddy <eddyjohn.terosier@hotmail.com>
2022-12-07 14:16:55 +0100

    streams in practice 2

--- 1-node-farm/streams.js
+++ 1-node-farm/streams.js
@@ -1,9 +1,24 @@
 const fs = require("fs")
 const server = require("http").createServer();
+
 server.on("request", (req,res)=>{
-    fs.readFile("test-file.txt", (err,data)=>{
-        if (err) console.log(err);
-        res.end(data)
+    // // SOLUTION 1
+    // fs.readFile("test-file.txt", (err,data)=>{
+    //     if (err) console.log(err);
+    //     res.end(data)
+    // })
+    // SOLUTION 2 Streams
+    const readable = fs.createReadStream("test-file.txt")
+    readable.on("data", chunk =>{
+        res.write(chunk);
+    })
+    readable.on("end", ()=>{
+        res.end();
+    })
+    readable.on("error", err =>{
+        console.log(err);
+        res.statusCode=500;
+        res.end("file not found")
     })
 })
 
