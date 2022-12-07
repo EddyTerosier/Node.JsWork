db91ad0ec837ecb4bd8f4526d8f119d3009bef71
Eddy <eddyjohn.terosier@hotmail.com>
2022-12-07 16:30:40 +0100

    requiring module in practice

--- 1-node-farm/module.js
+++ 1-node-farm/module.js
@@ -1,6 +1,17 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
 
+// MODULE.EXPORTS
 const C = require("./test-module-1");
 const calc1 = new C();
 console.log(calc1.add(2, 5));
+
+// EXPORTS
+// const calc2 = require("./test-module-2");
+const { add,multiply,divide } = require("./test-module-2");
+console.log(add(9, 9));
+console.log(multiply(9,9));
+console.log(divide(9,9));
+
+// CACHING
+require("./test-module-3")();
